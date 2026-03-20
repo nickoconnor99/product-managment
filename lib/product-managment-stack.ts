a0001feb4b6f2998c57540c3d7b34f19ee8fd1b3
Paul Allen <nickoconnor@gmail.com>
2026-03-20 22:12:14 +0000

    create product managment stack and lambda's

---
+++ lib/product-managment-stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as apigatewayv2_integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambdaRuntime from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
+
+export class ProductManagmentStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    const productsTable = new dynamodb.Table(this, `${this.stackName}-Products-Table`, {
+      tableName: `${this.stackName}-Products-Table`,
+      partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
+    const productImagesBucket = new s3.Bucket(this, `${this.stackName}-Product-Images-Bucket`, {
+      bucketName: `${this.stackName.toLocaleLowerCase()}-images`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    })
+
+    const createProductLambda = new NodejsFunction(this, `${this.stackName}-create-product`, {
+      runtime: lambdaRuntime.Runtime.NODEJS_22_X,
+      handler: 'handler',
+      entry: path.join(__dirname, '../src/lambda/products/createProduct.ts'),
+      functionName: `${this.stackName}-create-product-lambda`,
+      environment: {
+        PRODUCTS_TABLE_NAME: productsTable.tableName,
+        PRODUCT_IMAGES_BUCKET_NAME: productImagesBucket.bucketName,
+      },
+      timeout: cdk.Duration.seconds(60)
+    })
+   
+  }
+}
+
